02cf9834157f5ef34b06cb093054f2caec88167d
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2022-05-06 12:34:04 +0900

    huff test

--- util-compress.h
+++ util-compress.h
@@ -4,21 +4,70 @@
 #include <inttypes.h>
 
 #ifdef __cplusplus
+extern "C++" {
+/**
+ * Logger compress
+ * @param outStrm output stream
+ * @param srcBuffer source
+ * @param srcSize szie
+ * @return bits in the stream
+ */
+size_t compressLogger(
+        std::ostream &outStrm,
+        const void *srcBuffer,
+        size_t srcSize
+);
+
+/**
+ * Logger decompress
+ * @param outStrm output stream
+ * @param srcBuffer source
+ * @param srcSize szie
+ * @return bits in the stream
+ */
+size_t decompressLogger(
+        std::ostream &outStrm,
+        const void *srcBuffer,
+        size_t srcSize
+);
+
+/**
+ * Logger compress string
+ * @param value source
+ * @return compressed string
+ */
+std::string compressLoggerString(
+        const std::string &value
+);
+
+/**
+ * Logger decompress string
+ * @param value source
+ * @return decompressed string
+ */
+std::string decompressLoggerString(
+        const std::string &value
+);
+
+}
+#endif
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
-uint16_t compressLogger(
+size_t compressLoggerBuffer(
     char *outBuffer,
     size_t outSize,
-    const char *inBuffer,
-    uint16_t inSize
+    const char *srcBuffer,
+    uint16_t srcSize
 );
 
-uint16_t decompressLogger(
+size_t decompressLoggerBuffer(
     char *outBuffer,
     size_t outSize,
-    const char *inBuffer,
-    uint16_t inSize
+    const char *srcBuffer,
+    uint16_t srcSize
 );
 
 #ifdef __cplusplus
